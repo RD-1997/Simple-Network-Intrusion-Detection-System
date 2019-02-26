cd0a1e2d114866d46608e0c0a29a401811eb60c1
Hisk58 <47060156+Hisk58@users.noreply.github.com>
2019-02-26 22:34:58 +0100

    Update sniff.py
    updated for arguments

--- sniff.py
+++ sniff.py
@@ -8,4 +8,14 @@ def printinfo(pkt):
     if UDP in pkt:
         print("Source Port: " + str(pkt[UDP].sport) + " " + "Destination Port: " + str(pkt[UDP].dport) + "Protocol: [UDP]")
 
-sniff(iface="ens33",filter="ip and tcp or udp",prn=printinfo)
+# check if the arguments are filled in
+if len(sys.argv) != 2:
+    print("There are missing arguments. Usage: Python3 %s <network interface>"%(sys.argv[0]))
+    sys.exit(0)
+
+# defining the command line arguments
+interface = str(sys.argv[1])
+# interval = str(sys.argv[2])
+# collector = str(sys.argv[3])
+
+sniff(iface=interface,filter="ip and tcp or udp",prn=printinfo)
