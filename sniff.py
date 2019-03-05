5f84adbfc3de730ef3246ed781ac3b3ea0f1d88f
root <hakankece58@gmail.com>
2019-03-05 14:07:28 -0800

    Update pkg counter

--- sniff.py
+++ sniff.py
@@ -1,15 +1,22 @@
 from scapy.all import *
 
+### variables used widely ###
+countTCP = 0
+countUDP = 0
+
 def printinfo(pkt):
+    global countTCP, countUDP
     if IP in pkt:
         print("Source IP: " + str(pkt[IP].src) + " " + "Destination IP: " + str(pkt[IP].dst) +
               " Package in Bytes: " + pkt.sprintf("%IP.len%"))
     if TCP in pkt:
+        countTCP +=1
         print("Source Port: " + str(pkt[TCP].sport) + " " + "Destination Port: " +
-              str(pkt[TCP].dport) + " Protocol: [TCP]")
+              str(pkt[TCP].dport) + " Protocol: [TCP]" + " | " + str(countTCP))
     if UDP in pkt:
+        countUDP +=1
         print("Source Port: " + str(pkt[UDP].sport) + " " + "Destination Port: " +
-              str(pkt[UDP].dport) + " Protocol: [UDP]")
+              str(pkt[UDP].dport) + " Protocol: [UDP]" + " | " + str(countUDP))
 
 # check if the arguments are filled in
 if len(sys.argv) != 2:
@@ -21,4 +28,7 @@ interface = str(sys.argv[1])
 # interval = str(sys.argv[2])
 # collector = str(sys.argv[3])
 
-sniff(iface=interface,filter="ip and tcp or udp",prn=printinfo)
+
+print("[*] Start sniffing...")
+sniff(iface=interface,filter="ip and tcp or udp",prn=printinfo,count=10)
+print("[*] Stop sniffing")
--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 from scapy.all import *
 
-def printinfo(pkt):
-    if IP in pkt:
-        print(pkt.sprintf("%IP.len%"))
-
-
-sniff(iface="ens33",filter="ip",prn=printinfo)
