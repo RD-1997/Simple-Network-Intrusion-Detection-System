922ff96add94ab4fbfc977d6582df265525a6854
root <hakankece58@gmail.com>
2019-03-10 08:06:23 -0700

    app.py + flask + tiny update, nothing special

--- sniff.py
+++ sniff.py
@@ -1,34 +1,59 @@
 from scapy.all import *
-
-### variables used widely ###
-countTCP = 0
-countUDP = 0
-
-def printinfo(pkt):
-    global countTCP, countUDP
-    if IP in pkt:
-        print("Source IP: " + str(pkt[IP].src) + " " + "Destination IP: " + str(pkt[IP].dst) +
-              " Package in Bytes: " + pkt.sprintf("%IP.len%"))
-    if TCP in pkt:
-        countTCP +=1
-        print("Source Port: " + str(pkt[TCP].sport) + " " + "Destination Port: " +
-              str(pkt[TCP].dport) + " Protocol: [TCP]" + " | " + str(countTCP))
-    if UDP in pkt:
-        countUDP +=1
-        print("Source Port: " + str(pkt[UDP].sport) + " " + "Destination Port: " +
-              str(pkt[UDP].dport) + " Protocol: [UDP]" + " | " + str(countUDP))
+import json
+import time
 
 # check if the arguments are filled in
-if len(sys.argv) != 2:
-    print("There are missing arguments. Usage: Python3 %s <network interface>"%(sys.argv[0]))
+if len(sys.argv) != 3:
+    print("There are missing arguments. Usage: Python3 %s <network interface> <interval in minutes>"%(sys.argv[0]))
     sys.exit(0)
 
 # defining the command line arguments
-interface = str(sys.argv[1])
-# interval = str(sys.argv[2])
+interface = str(sys.argv[1])        # user specifies the network interface
+interval = str(sys.argv[2])         # user specifies the interval time
+intervalInt = int(interval) * 60    # converts the minutes given by the user into seconds and creates an integer
 # collector = str(sys.argv[3])
 
+# widely used variables
+countTCP = 0                        # variable to count the TCP packets
+countUDP = 0                        # variable to count the UDP packets
+countByte = 0                       # variable to count the packet size in bytes
+packetTime = str(time.time())       # variable to determine packet time in UTC format
 
-print("[*] Start sniffing...")
-sniff(iface=interface,filter="ip and tcp or udp",prn=printinfo,count=10)
-print("[*] Stop sniffing")
+# function for the scan details
+def printinfo(pkt):
+    global countTCP, countUDP
+
+    if IP in pkt:
+        print(str(pkt[IP].src) + ":" + str(pkt[IP].sport) + " --> " + str(pkt[IP].dst) + ":" + str(pkt[IP].dport) + " | ", end='')
+
+    if TCP in pkt:
+        countTCP +=1
+        print("TCP" + " | " + " UTC Timestamp: " + packetTime + " | TCP Packet number: " + str(countTCP), end='')
+        print("")
+
+    if UDP in pkt:
+        countUDP +=1
+        print("UDP" + " | "  + " UTC Timestamp: " + packetTime + " | UDP Packet number: " + str(countUDP), end='')
+        print("")
+
+
+# function to activate the scanner
+def sniffThatSh():
+    packetList = []     # creates a list where the scan results can be saved
+    print("[*] Start sniffing...\n")
+    packetList = sniff(iface=interface,filter="ip and tcp or udp",prn=printinfo, timeout=intervalInt)
+    print("[*] Stop sniffing\n")
+
+    global countTCP
+    global countUDP
+    countTCP = 0        # resets the TCP count
+    countUDP = 0        # resets the UDP count
+
+    print("Sending information to collector....\n")
+    print(packetList)   # shows list details
+    time.sleep(5)       # sleeps for 5 seconds
+
+    sniffThatSh()       # repeats the code
+
+
+sniffThatSh()           # executing the code
--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-from scapy.all import *
+from bson import json_util
+from pymongo import MongoClient
+import json
 
+### Steps for test ###
+# Test connection
+# make a sample data in JSON
+# put data into MongoDB
+# Take data from MongoDB
+
+# test connection of database
+try:
+    client = MongoClient('mongodb://localhost:27017/')
+    client.server_info()
+except pymongo.errors.ConnectionFailure as err:
+    print(err)
+
+# try json
+file = open('test.txt', 'w')
+file.write("Hello1")
+file.close()
